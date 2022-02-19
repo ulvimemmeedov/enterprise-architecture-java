2752035f8f128f502ae79130aea79267156f2b76
Ulvi Memmedov <onezerolord@gmail.com>
2022-02-19 23:00:34 +0400

    first commit

---
+++ src/nLayeredDemo/entities/concretes/Product.java
@@ -0,0 +1,64 @@
+package nLayeredDemo.entities.concretes;
+
+public class Product {
+    /* keys product */
+    private int id;
+    private int categoryId;
+    private String name;
+    private double unitPrice;
+    private double unitsInStock;
+
+    /* constructors */
+    public Product() {/* empty constructor */}
+
+    public Product(int id, int categoryId, String name, double unitPrice, double unitsInStock) {
+        super();
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.unitsInStock = unitsInStock;
+    }
+
+    /* Getter Setter */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(double unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+}
